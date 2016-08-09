6e8a8445e2162daff78bc4c51f09ca7f275e78d8
Luis Lithgow <lalithgow@gmail.com>
2016-08-09 14:57:11 -0400

    start of working with gpio

--- server.js
+++ server.js
@@ -39,130 +39,104 @@ app.get('/off', function(req,res) {
   state = false
 } );
 
-// Do we want the sequence to loop?
-let loop = true;
+// // Do we want the sequence to loop?
+// let loop = true;
 
-// Create a simple demo sequece that calls various
-// five.Led methods with specified arguments and
-// let it run for the given duration (defaults to 3 seconds).
-let demoSequence = [{
-  method: "pulse",
-  args: [1000],
-  duration: 5000
-}, {
-  method: "strobe",
-  args: [500],
-  duration: 3000
-}, {
-  method: "fadeIn",
-  args: [
-    2000,
-    function() {
-      console.log("fadeIn complete!");
-    }
-  ],
-  duration: 2500
-}, {
-  method: "fadeOut",
-  args: [
-    5000,
-    function() {
-      console.log("fadeOut complete!");
-    }
-  ],
-  duration: 5500
-}, {
-  method: "brightness",
-  args: [10],
-  duration: 3000
-}, {
-  method: "off"
-}];
-
-// $(".on").on("click",
-// if (state === true) {
-  console.log("will turn on")
-// Execute a method in the demo sequence
-function execute(step) {
-    console.log("clicked on")
-
-  // Grab everything we need for this step
-  let method = demoSequence[step].method;
-  let args = demoSequence[step].args;
-  let duration = demoSequence[step].duration || 3000;
-
-  // Just print out what we're executing
-  console.log("led." + method + "(" + (args ? args.join() : "") + ")");
-
-  // Make the actual call to the LED
-  five.Led.prototype[method].apply(led, args);
-
-  // Increment the step
-  step++;
+// // Create a simple demo sequece that calls various
+// // five.Led methods with specified arguments and
+// // let it run for the given duration (defaults to 3 seconds).
+// let demoSequence = [{
+//   method: "pulse",
+//   args: [1000],
+//   duration: 5000
+// }, {
+//   method: "strobe",
+//   args: [500],
+//   duration: 3000
+// }, {
+//   method: "fadeIn",
+//   args: [
+//     2000,
+//     function() {
+//       console.log("fadeIn complete!");
+//     }
+//   ],
+//   duration: 2500
+// }, {
+//   method: "fadeOut",
+//   args: [
+//     5000,
+//     function() {
+//       console.log("fadeOut complete!");
+//     }
+//   ],
+//   duration: 5500
+// }, {
+//   method: "brightness",
+//   args: [10],
+//   duration: 3000
+// }, {
+//   method: "off"
+// }];
 
 
-  // If we're at the end, start over (loop==true) or exit
-  if (step === demoSequence.length) {
-    if (loop) {
-      step = 0;
-    } else {
-      // We're done!
-      process.exit(0);
-    }
-  }
 
-  // Recursively call the next step after specified duration
-  board.wait(duration, function() {
-    execute(step);
-  });
-}
-// )
-// ENd of click function
+//   console.log("will turn on")
+// // Execute a method in the demo sequence
+// function execute(step) {
+//     console.log("clicked on")
 
-board.on("ready", function() {
-  // Defaults to pin 11 (must be PWM)
-  led = new five.Led(process.argv[2] || 11);
+//   // Grab everything we need for this step
+//   let method = demoSequence[step].method;
+//   let args = demoSequence[step].args;
+//   let duration = demoSequence[step].duration || 3000;
 
-  // Kick off the first step
-  execute(0);
-});
+//   // Just print out what we're executing
+//   console.log("led." + method + "(" + (args ? args.join() : "") + ")");
 
-// } else {
-//   console.log(false)
-//   state = false
+//   // Make the actual call to the LED
+//   five.Led.prototype[method].apply(led, args);
+
+//   // Increment the step
+//   step++;
+
+
+//   // If we're at the end, start over (loop==true) or exit
+//   if (step === demoSequence.length) {
+//     if (loop) {
+//       step = 0;
+//     } else {
+//       // We're done!
+//       process.exit(0);
+//     }
+//   }
+
+//   // Recursively call the next step after specified duration
+//   board.wait(duration, function() {
+//     execute(step);
+//   });
 // }
 
 
 
-
-
-
-
-// http.createServer(function (req, resp) {
-//   if (req.url === '/') {
-//     if (req.method === 'PUT') {
-//       req.pipe(request.put('http://mysite.com/doodle.png'))
-//     } else if (req.method === 'GET' || req.method === 'HEAD') {
-//       request.get('http://mysite.com/doodle.png').pipe(resp)
-//     }
-//   }
-// }).listen(3000)
-// console.log("server started rewuest")
-
-
-
-
-
-
-// let five = require("johnny-five");
-// let =board = new five.Board();
-// let led  ;
-// let toggleState = false;
-
-
 // board.on("ready", function() {
-//   console.log("The board is blinking!");
-//   let led = new five.Led(13);
-//   led.blink(500);
+//   // Defaults to pin 11 (must be PWM)
+//   led = new five.Led(process.argv[2] || 11);
+
+//   // Kick off the first step
+//   execute(0);
 // });
 
+
+
+
+
+// ******FOR RPI-GPIO******
+
+var pin = 11;
+
+gpio.setup(pin, gpio.OUT, function() {
+  gpio.write(pin, true, function(err) {
+    if (err) throw err;
+  });
+});
